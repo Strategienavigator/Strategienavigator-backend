4e78522fd9427933b1d1f0816a085d8eb100f184
Geist5000 <geist5000@gmail.com>
2021-06-23 17:03:15 +0200

    added tools and save tables

---
+++ database/migrations/2021_06_23_144709_create_save_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('save', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('last_opened')->nullable();
+            $table->jsonb('data')->nullable();
+            $table->foreignId('tool_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('save');
+    }
+}
