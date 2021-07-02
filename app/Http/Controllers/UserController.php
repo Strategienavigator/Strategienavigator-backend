1f7400117e6947e2dc665196eb1464dab9589264
Geist5000 <geist5000@gmail.com>
2021-07-02 17:00:31 +0200

    updated README

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -10,11 +11,11 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        return response()->json(User::all());
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -82,6 +82,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'last_login' => 'datetime',
+        'anonym' => 'boolean'
     ];
 
     public function saves(): \Illuminate\Database\Eloquent\Relations\HasMany
