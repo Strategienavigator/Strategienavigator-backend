6aa12a6d6cd79e39f3876c9fcc6da79ed5a30287
Geist5000 <geist5000@gmail.com>
2021-07-07 15:16:07 +0200

    SaveResource

---
+++ app/Http/Resources/SaveResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SaveResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "data"=>$this->data,
+            "locked_by"=>$this->locked_by_id,
+            "last_locked"=>$this->last_locked,
+            "owner_id"=>$this->owner_id,
+            "tool_id"=>$this->tool_id,
+        ];
+    }
+}
