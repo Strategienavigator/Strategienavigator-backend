4db70309395138f002ca4659559ee768c643499e
Geist5000 <geist5000@gmail.com>
2021-10-06 18:06:06 +0200

    added settings seeder to default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ToolSeeder::class,
             UserSeeder::class,
+            SettingsSeeder::class,
         ]);
     }
 }
