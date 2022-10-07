53a96dfb94f25450a57622d7b9ac3494776685a8
Geist5000 <geist5000@gmail.com>
2022-10-07 14:44:07 +0200

    added portfolio setting to settingsSeeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -23,6 +23,14 @@ class SettingsSeeder extends Seeder
                 "type" => "toggle",
                 "extras" => null,
                 "default" => "false"
+            ],
+            [
+                "id" => 2,
+                "name" => "Portfolio-Analyse Quadranten",
+                "description" => "Es lassen sich die Beschriftung der Quadranten einstellen oder komplett ausblenden.",
+                "type" => "portfolio-quadrants",
+                "extras" => null,
+                "default" => '{"toggled":false,"quadrants":[{"value":"Oben Links","header":"Oben Links"},{"value":"Oben Rechts","header":"Oben Rechts"},{"value":"Unten Links","header":"Unten Links"},{"value":"Unten Rechts","header":"Unten Rechts"}]}'
             ]
         ]);
     }
