e9b8cd71783836b08c879df24a12f01a898fc692
Geist5000 <geist5000@gmail.com>
2023-12-23 20:24:31 +0100

    made the captcha image bigger

--- config/captcha.php
+++ config/captcha.php
@@ -5,8 +5,8 @@ return [
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
         'length' => 9,
-        'width' => 120,
-        'height' => 36,
+        'width' => 150,
+        'height' => 45,
         'quality' => 90,
         'math' => false,
         'expire' => 60,
@@ -14,8 +14,8 @@ return [
     ],
     'math' => [
         'length' => 9,
-        'width' => 120,
-        'height' => 36,
+        'width' => 150,
+        'height' => 45,
         'quality' => 90,
         'math' => true,
     ],
