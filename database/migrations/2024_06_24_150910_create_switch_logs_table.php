b3271a59e7fda8c87efae205fb16a8f7f9ae624b
Omar kanoune <38401772+omarKanoune@users.noreply.github.com>
2024-10-08 16:06:46 +0200

    I have removed onDelete('cascade') to keep logs even when users delete their accounts.

--- database/migrations/2024_06_24_150910_create_switch_logs_table.php
+++ database/migrations/2024_06_24_150910_create_switch_logs_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('switch_logs', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->timestamp('action_time')->useCurrent();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
