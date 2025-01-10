3ae39c0e7c9fb4a6220657833112a3291180d45a
Gesit <geist5000@gmail.com>
2025-01-10 10:58:13 +0100

    fixed id column of roles table

--- database/migrations/2024_04_02_063314_create_roles_table.php
+++ database/migrations/2024_04_02_063314_create_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->integerIncrements('id');
+            $table->id();
             $table->string('name');
             $table->text('description');
             $table->timestamps();
