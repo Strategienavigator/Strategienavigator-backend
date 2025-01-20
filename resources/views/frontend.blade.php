d4b01f531a013f1218f1dfe7af977834c160323f
Gesit <geist5000@gmail.com>
2025-01-20 12:32:54 +0100

    reverted changes to frontend.blade.php

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -1,1 +1,27 @@
-<!doctype html><html lang="de"><head><meta charset="utf-8"/><meta content="width=device-width,initial-scale=1" name="viewport"/><meta content="#000000" name="theme-color"/><meta content="Dient zum Erstellen einfacher strategischer Werkzeuge." name="description"/><base href="/"><link href="/manifest.json" rel="manifest"/><title>Strategienavigator</title><link href="/favicon.ico" rel="icon" sizes="144x144"><script defer="defer" src="/static/js/main.7d1d9f2d.js"></script><link href="/static/css/main.294ffec6.css" rel="stylesheet"></head><body class="light" id="body"><noscript>Sie benötigen Javascript um diese Seite zu benutzen.</noscript><div id="root"></div><div id="messages"></div></body></html>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kein Frontend vorhanden</title>
+</head>
+<body>
+
+<header><h1>Kein Frontend verfügbar.</h1></header>
+
+<main>
+    <p>
+        <strong>Ups... da ist dem Admin wohl ein Fehler unterlaufen. Bitte kontaktiere <a
+                href="mailto:{{config("mail.from.address")}}?subject=Fehlendes%20Frontend&body=Sehr%20geehrtes%20{{config("app.name")}}-Team,%0A%0Aich%20muss%20ihnen%20leider%20mitteilen,%20dass%20sie%20auf%20Ihrer%20Website%20kein%20Frontend%20hochgeladen%20haben.%0A%0AMit%20freundlichen%20Gr%C3%BC%C3%9Fen%0A%0Aein%20Besucher">{{config("mail.from.address")}}</a>
+            damit der Fehler möglichst schnell behoben wird.</strong>
+    </p>
+
+    <p>
+        Danke für die Hilfe!
+    </p>
+</main>
+
+</body>
+</html>
