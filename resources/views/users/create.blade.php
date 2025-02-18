9f00bc3afb64593970f076d6d399cfd68ca91c3b
Omar kanoune <38401772+omarKanoune@users.noreply.github.com>
2025-02-18 11:37:09 +0100

    Implement error handling in admin user creation form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -16,6 +16,15 @@
 
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <form action={{ route('admin.users.store') }} method="post">
             @csrf
             <div class="mb-3">
